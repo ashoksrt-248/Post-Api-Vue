4a947b5a624966284317cadd6aa94d0b35317a83
Ashok Amara <ashok.amara@cesneosilica.com>
2024-04-08 13:33:29 +0530

    post api created

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>Post Api Call in VueJs</h1>
+    <PostApi />
+  </div>
+</template>
+
+<script>
+import PostApi from './components/PostApi.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostApi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostApi.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <form @submit.prevent="createPost">
+      <div>
+        <label for="userId">UserID</label>
+        <input type="text" id="userId" v-model="FormData.userId"/>
+      </div>
+      <div>
+        <label for="title">Title</label>
+        <input type="text" id="title" v-model="FormData.title"/>
+      </div>
+      <div>
+        <label for="body">Body</label>
+        <input type="text" id="body" v-model="FormData.body"/>
+      </div>
+      <button>Create Post</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+  export default {
+    name:'PostApi',
+    data(){
+      return{
+        FormData:{
+          userId:'',
+          title:'',
+          body:''
+        }
+      }
+    },
+    methods:{
+      createPost(){
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.FormData)
+        .then((response) => console.log(response))
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
