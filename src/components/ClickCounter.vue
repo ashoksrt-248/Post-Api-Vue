00e015d848e27ae7f7861bd5cd2f05f7eb2a2c40
Ashok Amara <ashok.amara@cesneosilica.com>
2024-04-08 18:01:38 +0530

    button styles added

--- src/components/ClickCounter.vue
+++ src/components/ClickCounter.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Counter App</h1>
-        <button @click="incrementCount">Clicked {{ count }} times</button>
+        <button @click="incrementCount" class="button"><span>Clicked<b class="bold"> {{ count }}</b> times</span></button>
     </div>
 </template>
 
@@ -14,5 +14,47 @@ import CounterMix from "../mixins/counter"
 </script>
 
 <style scoped>
+.bold{
+    color: teal;
+}
+ .button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 18px;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
 
 </style>
